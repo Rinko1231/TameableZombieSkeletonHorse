71004029f55db4b68b48161930d0e5bf6b4bc2c9
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-03-04 21:23:26 +0800

    Add files via upload

---
+++ src/main/java/com/rinko1231/tamablezombieskeletonhorse/TameableZombieSkeletonHorse.java
@@ -0,0 +1,13 @@
+package com.rinko1231.tamablezombieskeletonhorse;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("tameablezombieskeletonhorse")
+public class TameableZombieSkeletonHorse {
+
+
+    public TameableZombieSkeletonHorse() {
+
+        }
+
+}
