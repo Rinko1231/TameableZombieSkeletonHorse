6e5399303bbfa4352e90fa4c60298c9dbd7b2ccf
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-06-18 08:40:49 +0800

    Add files via upload

---
+++ src/main/java/com/rinko1231/tameablezombieskeletonhorse/mixin/SkeletonHorseMixin.java
@@ -0,0 +1,45 @@
+package com.rinko1231.tameablezombieskeletonhorse.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.animal.horse.SkeletonHorse;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = SkeletonHorse.class)
+public abstract class SkeletonHorseMixin extends AbstractHorse {
+
+    protected SkeletonHorseMixin(EntityType<? extends AbstractHorse> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
+    public void mobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        boolean flag = this.isTamed() && player.isSecondaryUseActive();
+        if (!this.isVehicle() && !flag) {
+            ItemStack itemstack = player.getItemInHand(hand);
+            if (!itemstack.isEmpty()) {
+                if (this.isFood(itemstack)) {
+                    cir.setReturnValue(this.fedFood(player, itemstack));
+                }
+
+                if (!this.isTamed()) {
+                    this.makeMad();
+                    cir.setReturnValue(InteractionResult.sidedSuccess(this.level().isClientSide));
+                }
+            }
+
+            cir.setReturnValue(super.mobInteract(player, hand));
+        } else {
+            cir.setReturnValue(super.mobInteract(player, hand));
+        }
+        cir.cancel();
+    }
+}
